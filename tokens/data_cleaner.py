5ff045b51a4da4db262d96b41497a827b5060864
pzn666 <pzn666@hotmail.com>
2017-10-20 18:40:02 +0800

    Add jieba filter to data cleaer

---
+++ tokens/data_cleaner.py
@@ -0,0 +1,82 @@
+from pymongo import MongoClient
+from pprint import pprint
+from token_base import TokenBase
+from word2vec import WordEmbedder
+from bson import Binary
+import pickle
+import os
+import re
+import timer
+
+mongo_config = {
+    'host': 'localhost',
+    'port': 27017,
+    'db': 'focal',
+    'data': 'news_data',
+    'train_data': 'key_train_data'
+}
+
+
+class DataCleaner:
+    def __init__(self, config):
+        self.config = config
+
+    @timer.clocktimer
+    def __extract_keywords(self):
+        client = MongoClient(self.config['host'], self.config['port'])
+        data_col = client[self.config['db']][self.config['data']]
+        train_col = client[self.config['db']][self.config['train_data']]
+        key_docs = data_col.find({'keyword': {'$nin': [{}, None]}})
+        for doc in key_docs:
+            pos = []
+            for key in doc['keyword']:
+                if key == 'keyword':
+                    for inner_key in doc['keyword']['keyword']:
+                        pos += doc['keyword']['keyword'][inner_key]
+                else:
+                    pos += doc['keyword'][key]
+                # for word in temp:
+                #     pos += word.split()
+                doc['custom_corpus'] = {
+                            'account': [],
+                            'project_name': [],
+                            'keyword': {
+                                'pos': list(set(pos)),
+                                'neg': []
+                            }
+                        }
+            yield doc
+
+    @timer.clocktimer
+    def __filter_by_jieba(self, docs_gen):
+        tokenizer = TokenBase()
+        for doc in docs_gen:
+            pos_set = set(doc['custom_corpus']['keyword']['pos'])
+            terms_set = set(
+                tokenizer.jieba_tokenize(doc['context'], ex_stop=True))
+            if bool(terms_set.intersection(pos_set)):
+                yield doc
+
+    @timer.clocktimer
+    def __insert_mongo(self, docs_gen, bulk=False):
+        client = MongoClient(self.config['host'], self.config['port'])
+        col = client[self.config['db']][self.config['train_data']]
+        if bulk:
+            bulk = col.initialize_unordered_bulk_op()
+            for key in docs:
+                bulk.find({'url': key}).upsert().replace_one(docs[key])
+            bulk.execute()
+        else:
+            col.insert_many(list(docs_gen))
+
+    @timer.clocktimer
+    def gen_train_data(self):
+        extracted_docs = self.__extract_keywords()
+        jieba_docs = self.__filter_by_jieba(extracted_docs)
+        print(len(list(jieba_docs)))
+        # result = self.__insert_mongo(docs)
+        print('Data cleanning done')
+
+
+gener = DataCleaner(mongo_config)
+gener.gen_train_data()
